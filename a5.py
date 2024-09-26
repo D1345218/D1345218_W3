8156499e27fa577dd78a0e2b29e659ea60c1ac94
OK <D1345218@o365.fcu.edu.tw>
2024-09-26 14:50:08 +0800

    a6.py file

--- a5.py
+++ a5.py
@@ -1,2 +1,5 @@
-a, b, c= map(int, input('輸入三位數字'))
-print(c,b,a)
+a = int (input('輸入三位數字')) 
+b = a%10
+d = a//100
+c= a//10 - d*10
+print('結果:',b,c,d,sep="")
--- a6.py
+++ a6.py
@@ -1,6 +1,7 @@
-a = int (input('輸入係數a'))
-b = int (input('輸入係數b'))
-c = int (input('輸入係數c'))
-d =  ax^2 + bx + c = 0 
-e = -b ± b ** 2 - 4 * a * c **0.5 / a * 2
-print(d,'方程式的根為')
+import math
+a = int (input('輸入係數a:'))
+b = int (input('輸入係數b:'))
+c = int (input('輸入係數c:'))
+d = (-b + (b **2 -4*a*c)**0.5) /2*a
+e = (-b - (b **2 -4*a*c)**0.5) /2*a
+print('方程式的根為:','x1=',d,'x2=',e)
